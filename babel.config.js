263642a58c46f8ce38b47ca5a907be470bcdd13e
ldriveron <ldriveron@gmail.com>
2020-02-01 20:48:07 -0500

    Production Build Improvements
    -Fixed an issue where the application would not load on Internet Explorer.
    -Fixed a bug that disabled the Set Mood button for new accounts if they did not provide a day recap.
    -Empty day spots on the monthly calendary now works on mobile and Firefox.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-	presets: [ '@babel/preset-env', '@babel/preset-react' ],
+	presets: [
+		[ '@babel/preset-env', { targets: { browsers: [ 'last 1 version', 'ie >= 11' ] } }, '@babel/preset-react' ]
+	],
 	plugins: [ '@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties' ]
 };
